e5bc544a93f43f30e0047cc13e42cf782f97a038
Octa <octaviotorres115@gmail.com>
2022-02-07 19:11:24 -0300

    Minimos cambios

--- js/templatemo-script.js
+++ js/templatemo-script.js
@@ -1,5 +1,5 @@
 const initBg = (autoplay = true) => {
-    const bgImgsNames = ['imagen principal.png', 'ppal.1.jpg', 'ppal.2.jpg'];
+    const bgImgsNames = ['imagen principal.png', 'ppal.1.jpg', 'imagenprincipal3.png'];
     const bgImgs = bgImgsNames.map(img => "img/" + img);
 
     $("#demo").backstretch(bgImgs, {duration: 3000, fade: 500});
